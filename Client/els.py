529b7f2bd66eae11b56b963626a4714d5521dd8f
Mohamed Mohamed Ibrahim <mohamedgawad600@gmail.com>
2025-05-25 11:40:06 +0300

    adding argparse to pythonscript

--- Client/els.py
+++ Client/els.py
@@ -1,13 +1,18 @@
+import argparse
 import pandas as pd
 from elasticsearch import Elasticsearch, helpers
 
-# Connect to Elasticsearch (update with your credentials if needed)
-es = Elasticsearch("https://localhost:9200",
-                    # use_ssl = False,
-                    http_auth=("elastic", "49EO-a9NAqytA1Mnt8gB"),
-                    # compatible_with=7,
-                    # ca_certs=False,
-                    verify_certs=False)
+# Set up argument parser
+parser = argparse.ArgumentParser(description="Load Parquet data to Elasticsearch.")
+parser.add_argument('--password', required=True, help="Password for Elasticsearch")
+args = parser.parse_args()
+
+# Connect to Elasticsearch using the password argument
+es = Elasticsearch(
+    "https://localhost:9200",
+    http_auth=("elastic", args.password),
+    verify_certs=False
+)
 
 # Read your Parquet file
 df = pd.read_parquet("/mnt/01D8D4FB872972F0/Life/collage/collage_labs/year_3/term2/DDIA/project/Weather-Stations-Monitor/Backend/BaseCentralStation/Parquet/0.parquet")
@@ -21,6 +26,6 @@ actions = [
     for _, row in df.iterrows()
 ]
 
-# Bulk load data to ES
+# Bulk load data to Elasticsearch
 helpers.bulk(es, actions)
 print("Data loaded successfully!")
